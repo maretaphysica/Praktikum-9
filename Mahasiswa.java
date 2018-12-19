10a704ecce8eaac29a90e3bb9517f9dccb15c239
Mareta Physica <36698449+maretaphysica@users.noreply.github.com>
2018-12-19 07:34:46 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,23 @@
+package Praktikum_9;
+
+public class Mahasiswa {
+    int nim;
+    String nama_mhs;
+    public Mahasiswa(int n){
+        
+    }
+    public void setNim(int ni){
+        
+    }
+    public int getNim(){
+        return nim;
+    }
+    
+    public void setNama(String na){
+        
+    }
+    public String getNama(){
+        return nama_mhs;
+    }
+    
+}
---
+++ Truk.java
@@ -0,0 +1,26 @@
+package Praktikum_9;
+
+public class Truk {
+    double muatan;
+    double muatanmaks;
+    
+    public Truk(double beratmaks){
+        this.muatanmaks= beratmaks;
+    }
+    
+    public double getMuatan(){
+        return muatan;
+    }
+    
+    public double getMuatanMaks(){
+        
+        return muatanmaks;
+        
+    }
+    
+    public void tambahMuatan(double berat){
+         if((berat+muatan)<=muatanmaks){
+             muatan=muatan+berat;
+         }
+    }
+  }
